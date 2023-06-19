86cfe3119d78c8a9ebcfd52097df997d91df2803
Preetisam <preetikamilla@gmail.com>
2023-06-19 21:59:57 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,45 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import './App.css'
 
-function App() {
-  const [list, setList] = useState([]);
+
+const Table = () => {
+  const search = useRef();
+  const [data, setData] = useState([]);
+  const [order, setOrder] = useState(true);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const orderFn = (str) => {
+    if (str === 'dec') {
+      setData([...data].sort((a, b) => b.id - a.id));
+      setOrder(false);
+    } else if (str === 'asc') {
+      setData([...data].sort((a, b) => a.id - b.id));
+      setOrder(true);
+    }
+  };
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => setList(data));
+    getData();
   }, []);
-
-  const handleSort = () => {
-    const sortedData = [...list].sort((b, a) => a.id.localeCompare(b.id));
-    setList(sortedData);
-  };
 
   return (
     <>
-    <button onClick={handleSort}> sorting ascending</button>
-    <table>
-      <thead>
-        <tr>
-         
-          <th>userId</th>
-          <th>id</th>
-          <th>title</th>
-          <th>body</th>
-        </tr>      
-      </thead>
-      <tbody>
-        {list.map(id => (
-          <tr key={id.id}>
-            <td>{id.userId}</td>
-            <td>{id.id}</td>      
-            <td>{id.title}</td>      
-            <td>{id.body}</td>      
+      <div>
+        {order ? (
+          <button onClick={() => orderFn('dec')}>sorting descending</button>
+        ) : (
+          <button onClick={() => orderFn('asc')}>sorting ascending</button>
+        )}
+        <br/>
+        <input type='text' ref={search} placeholder='Search by title' onChange={() => console.log(search.current.value)} />
+      </div>
+      <table class="table table-success table-striped">
+        <thead>
+          <tr>
+            <th >id</th>
+            <th>userId</th>
+            <th>title</th>
+            <th>body</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data
+            .filter((item) => item.title.includes(search.current?.value || ''))
+            .map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.userId}</td>
+                <td>{item.title}</td>
+                <td>{item.body}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </>
   );
-}
+};
 
-export default App;
+export default Table;
